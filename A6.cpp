1b870d9fa5941c0d8281851cd226b19d23126c50
TonyJM <53893112+TonyJM@users.noreply.github.com>
2019-10-19 01:06:42 +0530

    Update A6.cpp

--- A6.cpp
+++ A6.cpp
@@ -1,34 +1,41 @@
 #include <iostream>
 using namespace std;
 
-void displaySubset(int subSet[], int size) {
+void disp(int sub[], int size) {
    for(int i = 0; i < size; i++) {
-      cout << subSet[i] << "  ";
+      cout <<sub[i] << "  ";
    }
    cout << endl;
 }
 
-void subsetSum(int set[], int subSet[], int n, int subSize, int total, int nodeCount ,int sum) {
-   if( total == sum) {
-      displaySubset(subSet, subSize);     //print the subset
-      subsetSum(set,subSet,n,subSize-1,total-set[nodeCount],nodeCount+1,sum);     //for other subsets
-      return;
-   }else {
-      for( int i = nodeCount; i < n; i++ ) {     //find node along breadth
-         subSet[subSize] = set[i];
-         subsetSum(set,subSet,n,subSize+1,total+set[i],i+1,sum);     //do for next node in depth
-      }
-   }
+void solve(int a[],int b[],int j,int i,int n,int total,int sum){
+    if(total<sum){
+        b[i]=a[j];
+        total+=b[i];
+        solve(a,b,j+1,i+1,n,total,sum);
+        if(j+1<n){
+            total-=b[i];
+            solve(a,b,j+1,i,n,total,sum);
+        }
+    }
+    else{
+        if(total==sum){
+            disp(b,i);
+        }
+        return;
+    }
 }
 
-void findSubset(int set[], int size, int sum) {
-   int *subSet = new int[size];     //create subset array to pass parameter of subsetSum
-   subsetSum(set, subSet, size, 0, 0, 0, sum);
-   delete[] subSet;
-}
 
 int main() {
+    int sub[7];
+
    int weights[] = {10, 7, 5, 18, 12, 20, 15};
    int size = 7;
-   findSubset(weights, size, 35);
+   int sum=35;
+
+//   int weights[] = {3,5,21,78};
+//   int size = 4;
+//   int sum=15;
+   solve(weights,sub,0,0, size,0,sum);
 }
